bb16a6979664b3f7919191c778e3d27279ef21f3
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-27 10:33:29 +0100

    added comments

--- app.rb
+++ app.rb
@@ -1,26 +1,31 @@
 require 'sinatra'
 require_relative "./ToDoManager.rb"
 
+#Index - Show all the data
 get '/todos/' do
 	ToDoManager.index
 end
 
+#Create - Create a new todo
 post '/todos' do
 	new_todo = params[:todo]
 	ToDoManager.create new_todo
 end
 
+#Show - Show a single record
 get '/todos/:id' do
 	id = params[:id].to_i
 	ToDoManager.show id
 end
 
+#Update - Update a single record
 put '/todos/:id' do
 	id = params[:id].to_i
 	new_todo = params[:todo]
 	ToDoManager.update(id, new_todo)
 end
 
+#Destroy - Delete a single record
 delete '/todos/:id' do
 	id = params[:id].to_i
 	ToDoManager.delete id
